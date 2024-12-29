6550bb50fa90346ea467fd1c6c1ea743b365bafc
Muhammad Azam <mazam1573@gmail.com>
2024-12-29 19:31:06 +0500

    Setup Completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 // import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1 className="text-3xl text-white font-black">Welcome to nextjs 👏</h1>
-  );
+  return <h1 className="text-3xl font-black">Welcome to nextjs 👏</h1>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,5 +57,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [],
 } satisfies Config;
