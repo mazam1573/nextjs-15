419e37578cfce590f49374799a0e19e5d99be22a
Muhammad Azam <mazam1573@gmail.com>
2025-01-19 17:04:10 +0500

    mobile nav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,19 +1,10 @@
 // import Image from "next/image";
 import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import ROUTES from "@/constants/routes";
 
 const Home = async () => {
   const session = await auth();
-  console.log(session);
+  console.log("Loged-In", session);
   return (
     <>
       <h1 className="text-3xl font-black">Welcome to nextjs 👏</h1>
@@ -25,19 +16,7 @@ const Home = async () => {
           await signOut({ redirectTo: ROUTES.SIGN_IN });
         }}
       >
-        <Button type="submit">Log out</Button>
       </form>
-      <DropdownMenu>
-        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>Profile</DropdownMenuItem>
-          <DropdownMenuItem>Billing</DropdownMenuItem>
-          <DropdownMenuItem>Team</DropdownMenuItem>
-          <DropdownMenuItem>Subscription</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
     </>
   );
 };
--- constants/routes.ts
+++ constants/routes.ts
@@ -2,7 +2,12 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
-  NOT_FOUND: "/404",
+  COMUNITY: "/community",
+  COLLECTION: "/collection",
+  JOBS: "/find-jobs",
+  TAGS: "/tags",
+  PROFILE: "/profile",
+  ASK_QUESTION: "/ask-a-question",
 };
 
 export default ROUTES;
