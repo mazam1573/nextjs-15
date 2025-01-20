5a29518c11a517ac01102ae375983ebd233c5ea0
Muhammad Azam <mazam1573@gmail.com>
2025-01-21 04:35:11 +0500

    left sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/nvabar";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
-      <Navbar></Navbar>
-      {children}
+    <main className="background-light850_dark100 relative">
+      <Navbar />
+      <div className="flex">
+        <LeftSidebar />
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
     </main>
   );
 };
